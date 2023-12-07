5e3bcd26d23cda85de7537b08307f685c17f94e2
BenPotel <benjamin.potel@proton.me>
2023-12-07 13:21:24 +0100

    Login script and small tweaks to the login.css

---
+++ FrontEnd/assets/scripts/login.js
@@ -0,0 +1,35 @@
+async function login() {
+  const emailLogin = document.getElementById("email").value
+  const passwordLogin = document.getElementById("password").value
+
+  const user = {
+    email: emailLogin,
+    password: passwordLogin,
+  }
+
+  await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    body:JSON.stringify(user),
+  })
+    .then((response) =>{
+      if(response.ok) {
+        response.json()
+      .then((data) => {
+        const userdata = data.token
+        if (localStorage.user = userdata) 
+        document.location.href=("index.html")
+        })} else {
+          document.querySelector(".error").innerHTML = "Utilisateur inconnu ou mot de passe érroné"
+        }
+    })    
+}
+
+const submitForm = document.querySelector(".connexion")
+submitForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  login()
+})
